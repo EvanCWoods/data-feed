b98572e4a6869ef473a6a0ec2b980297ce604e41
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-04-16 20:28:58 +1000

    heroku deployemnt

---
+++ script.py
@@ -0,0 +1,39 @@
+import time
+import requests
+from pymongo import MongoClient
+from decouple import config
+
+
+# GLOBAL VARIABLES
+HOUR = 3600
+API_ENDPOINT="https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD"
+
+client = MongoClient(config("MONGO_URI"))
+db = client["myDatabase"]
+collection = db["live-data"]
+
+# Function to get the data from the api
+def getDataFeed(API_ENDPOINT):
+    data = requests.get(API_ENDPOINT)
+    data = data.json()
+    return data["USD"]
+
+
+# Recursive function to get the current unix timestamp
+def getHours():
+    currentTime = int(time.time())  # Get the current time as an interger
+
+    if (currentTime % HOUR == 0):   # If the time is devisable by the number of seconds in an hour, it is a new hour
+        collection.insert_one(
+            {
+                "USD": getDataFeed(API_ENDPOINT),
+                "Timestamp": currentTime
+            }
+        )
+        time.sleep(1)
+        getHours()
+    else:   # If the current time is not a new hour, re run the function, adding a 1 second delay
+        time.sleep(1)
+        getHours()
+
+getHours()
